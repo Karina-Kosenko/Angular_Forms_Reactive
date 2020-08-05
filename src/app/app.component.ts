5d2f9217ba0c6983c8ecbc2e96be486e3cebdd14
Karina-Kosenko <karina.kosenko@techs.com.ua>
2020-08-05 15:35:24 +0300

    Section-15-point-210

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,13 @@ import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signUpForm: FormGroup;
+  forbiddenUsernames = ['Anna', 'Max'];
 
   ngOnInit() {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null,
+          [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null,
           [Validators.required, Validators.email]),
       }),
@@ -34,4 +36,13 @@ export class AppComponent implements OnInit {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signUpForm.get('hobbies')).push(control);
   }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+
+    return null;
+  }
 }
+
