9d2f8951b483dc794b732225a9b67370d2729d54
Karina-Kosenko <karina.kosenko@techs.com.ua>
2020-08-05 15:50:55 +0300

    Section-15-point-212

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,8 @@ export class AppComponent implements OnInit {
         'username': new FormControl(null,
           [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null,
-          [Validators.required, Validators.email]),
+          [Validators.required, Validators.email],
+          [this.forbiddenEmails.bind(this)]),
       }),
       'gender': new FormControl('female'),
       'hobbies': new FormArray([])
@@ -43,6 +45,18 @@ export class AppComponent implements OnInit {
     }
 
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+   return new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
   }
 }
 
